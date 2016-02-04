380c564d34496d0825ace6a07ec7a4de8ccb19b6
Lionel Landwerlin <llandwerlin@gmail.com>
2016-02-04 17:38:59 +0000

    first pipeline

--- JsDataflow.js
+++ JsDataflow.js
@@ -1,2 +1,19 @@
 const Parser = imports.Parser;
 const Utils = imports.Utils;
+
+if (ARGV.length < 1)
+  throw new Error('Need at least one argument');
+
+let tree = Parser.DataflowParser.matchAll(Utils.loadFile(ARGV[0]), 'top');
+let nodeList = Parser.ExtractNodesTransform.match(tree, 'trans');
+log(JSON.stringify(nodeList));
+
+let nodes = [];
+for (let i = 0; i < nodeList.length; i++)
+  nodes.push(nodeList[i].name);
+log(nodes);
+
+let modified = Parser.SetNodesTransforms.createInstance();
+modified.setNodes(nodes);
+let ret = modified.match(tree, 'trans');
+log(JSON.stringify(ret));
