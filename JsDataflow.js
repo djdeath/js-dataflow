a992bfa93b2e68ceb4dbf173c62ccba99cd453dc
Lionel Landwerlin <llandwerlin@gmail.com>
2016-02-05 17:51:33 +0000

    Add builtin support to initial grammar/generator

--- JsDataflow.js
+++ JsDataflow.js
@@ -5,15 +5,9 @@ if (ARGV.length < 1)
   throw new Error('Need at least one argument');
 
 let tree = Parser.DataflowParser.matchAll(Utils.loadFile(ARGV[0]), 'top');
-let nodeList = Parser.ExtractNodesTransform.match(tree, 'trans');
-log(JSON.stringify(nodeList));
+log(tree);
 
-let nodes = [];
-for (let i = 0; i < nodeList.length; i++)
-  nodes.push(nodeList[i].name);
-log(nodes);
 
-let modified = Parser.SetNodesTransforms.createInstance();
-modified.setNodes(nodes);
-let ret = modified.match(tree, 'trans');
-log(JSON.stringify(ret));
+
+let js = Parser.JsGen.match(tree, 'trans');
+log(js);
