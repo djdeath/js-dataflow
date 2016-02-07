a3e51a33575160243f4deabf1bd8357850f8a0a0
Lionel Landwerlin <llandwerlin@gmail.com>
2016-02-07 23:28:42 +0000

    compiler working

--- JsDataflow.js
+++ JsDataflow.js
@@ -5,9 +5,8 @@ if (ARGV.length < 1)
   throw new Error('Need at least one argument');
 
 let tree = Parser.DataflowParser.matchAll(Utils.loadFile(ARGV[0]), 'top');
-log(tree);
+let nodes = Parser.ExtractNodes.match(tree, 'trans');
 
-
-
-let js = Parser.JsGen.match(tree, 'trans');
-log(js);
+let js = Parser.JsGen.createInstance();
+js.setInput(nodes.map(function(e) { return e.name; }));
+log(js.match(nodes, 'program'));
