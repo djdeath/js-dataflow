b5ae3b3a9113a35a1bf28eb64ae325c274c5660a
Lionel Landwerlin <llandwerlin@gmail.com>
2016-02-08 00:19:15 +0000

    basic working runtime

--- JsDataflow.js
+++ JsDataflow.js
@@ -1,12 +1,28 @@
 const Parser = imports.Parser;
 const Utils = imports.Utils;
 
+const DataflowRuntime = imports.JsDataflowRuntime;
+
 if (ARGV.length < 1)
   throw new Error('Need at least one argument');
 
-let tree = Parser.DataflowParser.matchAll(Utils.loadFile(ARGV[0]), 'top');
-let nodes = Parser.ExtractNodes.match(tree, 'trans');
+let translate = function(text) {
+  let tree = Parser.DataflowParser.matchAll(text, 'top');
+  let nodes = Parser.ExtractNodes.match(tree, 'trans');
 
-let js = Parser.JsGen.createInstance();
-js.setInput(nodes.map(function(e) { return e.name; }));
-log(js.match(nodes, 'program'));
+  let js = Parser.JsGen.createInstance();
+  js.setInput(nodes.map(function(e) { return e.name; }));
+
+  let ret = js.match(nodes, 'program');
+  return ret;
+};
+
+let toDataflow = function(text) {
+  let params = eval('(function(){return ' + translate(text) + ';})()');
+  return new DataflowRuntime.Dataflow({ nodes: params });
+};
+
+let text = Utils.loadFile(ARGV[0]);
+let flow = toDataflow(text);
+flow.start();
+log(JSON.stringify(flow.getValues()));
--- JsDataflowRuntime.js
+++ JsDataflowRuntime.js
@@ -3,24 +3,47 @@ const Lang = imports.lang;
 const Dataflow = new Lang.Class({
   Name: 'Dataflow',
 
-  init: function(args) {
+  _init: function(args) {
     this._nodes = {};
     for (let i = 0; i < args.nodes.length; i++) {
       let node = args.nodes[i];
       this._nodes[node.name] = {
         name: node.name,
-        eval: node.eval,
         children: [],
+        eval: node.eval,
+        inputs: node.inputs,
+        value: undefined,
       };
     }
     for (let i = 0; i < args.nodes.length; i++) {
       let node = args.nodes[i];
-      for (let j = 0; j < nodes.inputs; j++)
-        this._nodes[node.inputs[i]].children.push(node.name);
+      for (let j = 0; j < node.inputs.length; j++)
+        this._nodes[node.inputs[j]].children.push(node.name);
     }
+    log('->' + JSON.stringify(this._nodes));
+  },
+  _updateNode: function(node) {
+    log('update ' + node.name + ' children=' + node.children)
+    node.value = node.eval(this._nodes);
+    log('updated ' + node.name + ' value=' + node.value);
+    for (let i = 0; i < node.children.length; i++)
+      this._updateNode(this._nodes[node.children[i]]);
   },
   start: function() {
+    for (let n in this._nodes) {
+      let node = this._nodes[n];
+      if (node.inputs.length > 1)
+        continue;
+      this._updateNode(node);
+      node.value = node.eval(this._nodes);
+    }
   },
   stop: function() {
   },
+  getValues: function() {
+    let ret = {};
+    for (let n in this._nodes)
+      ret[n] = this._nodes[n].value;
+    return ret;
+  }
 });
