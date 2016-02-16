b9b4554afffa9b7484ad1e7959d5a5702dd8042e
Lionel Landwerlin <llandwerlin@gmail.com>
2016-02-16 01:10:47 +0000

    remove commented code

--- JsDataflow.js
+++ JsDataflow.js
@@ -6,20 +6,6 @@ const DataflowRuntime = imports.JsDataflowRuntime;
 if (ARGV.length < 1)
   throw new Error('Need at least one argument');
 
-// let translate = function(text) {
-//   let tree = Parser.DataflowParser.matchAll(text, 'top');
-//   let nodes = Parser.ExtractNodes.match(tree, 'trans');
-
-//   log('Nodes: ' + nodes.map(function(e) { return e.name; }));
-
-
-//   let js = Parser.DataflowJsGen.createInstance();
-//   js.setInput(nodes.map(function(e) { return e.name; }));
-
-//   let ret = js.match(nodes, 'program');
-//   return ret;
-// };
-
 let z = 43;
 
 let toDataflow = function(text) {
